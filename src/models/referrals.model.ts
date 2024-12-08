460a1706b0efc8a25c1946df425afa0433baffef
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-12-08 17:16:41 +0100

    feat: create referrals model

---
+++ src/models/referrals.model.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+export interface ReferralDocument extends mongoose.Document {
+    earnerId: mongoose.Schema.Types.ObjectId;
+    referredUserId: mongoose.Schema.Types.ObjectId;
+    email?: string;
+    status: "pending" | "accepted";
+    sentAt?: Date;
+    acceptedAt?: Date;
+    createdAt: Date;
+    updatedAt: Date;
+    __v?: number;
+}
+
+const referralSchema = new mongoose.Schema<ReferralDocument>(
+    {
+      earnerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      referredUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      email: {
+        type: String,
+        required: false,
+      },
+      status: {
+        type: String,
+        enum: ["pending", "accepted"],
+        default: "pending",
+        required: true,
+      },
+      sentAt: {
+        type: Date,
+        default: null,
+      },
+      acceptedAt: {
+        type: Date,
+        default: null,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  const ReferralModel = mongoose.model<ReferralDocument>("Referral", referralSchema);
+  export default ReferralModel;
